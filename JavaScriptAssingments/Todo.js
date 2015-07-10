f203231d8d15f9aa91181567de099d58812f3f2b
hemant.bonde <hemant.bonde@weboniselab.com>
2015-07-10 17:13:54 +0530

    JavaScript Assingments.

---
+++ JavaScriptAssingments/Todo.js
@@ -0,0 +1,94 @@
+(function() {
+
+	var todo = document.querySelector('#todolist'), form = document.querySelector('form'), field = document.querySelector('#newitem') ,
+	 tb = document.querySelector('#tb6'), todo1 = document.querySelector('#todolist1'), todo2 = document.querySelector('#todolist2'), 
+	 todo3 = document.querySelector('#todolist3');
+	
+	/*alert(todo);  */
+    
+
+	form.addEventListener('submit', function(ev) {
+		var a = field.value
+		todo.innerHTML += '<li>' + a + '</li> ';
+		todo2.innerHTML += '<li>' + a + '</li> ' ;
+		//todo3.innerHTML = '<li>' + a + '</li>';		
+		//alert(field.value);
+		field.value = '';
+		field.focus();
+		storestate();
+		
+		//todo3.innerHTML = '<li>' + a + '</li>';
+		ev.preventDefault();
+	}, false);
+    var temp;
+    function storestateCompleted(temp) {
+		
+		//alert("Are you Sure ? " + localStorage.todolist);	
+		todolist3.innerHTML  += '<li>' + temp + '</li> ';	
+	}
+	;
+	todo.addEventListener('click', function(ev) {
+		var t = ev.target;
+		if (t.tagName === 'LI') {
+			if (t.classList.contains('done')) {
+				temp= t.innerHTML;
+				storestateCompleted(temp);		
+
+				t.parentNode.removeChild(t);
+
+				
+			} else {
+				t.classList.add('done');
+			}
+			storestate();
+			todo1.innerHTML = '<li>' + localStorage.todolist; + '</li>';
+			//todo3.innerHTML = '<li>' + a + '</li>';
+		}
+		;
+		ev.preventDefault();
+	}, false);
+    
+
+
+    todo3.addEventListener('click', function(ev) {
+		var t = ev.target;
+		if (t.tagName === 'LI') {
+			if (t.classList.contains('done')) {
+				temp= t.innerHTML;
+				t.parentNode.removeChild(t);
+
+				
+			} else {
+				t.classList.add('done');
+			}
+			storestate();
+			}
+		;
+		ev.preventDefault();
+	}, false);
+
+	
+
+	document.addEventListener('DOMContentLoaded', retrievestate, false);
+
+	function storestate() {
+		localStorage.todolist = todo.innerHTML;
+		localStorage.todolist2 = todo2.innerHTML;
+		localStorage.todolist3 = todo3.innerHTML;
+		
+		//alert("Are you Sure ? " + localStorage.todolist);		
+	}
+	;
+	
+
+	function retrievestate() {
+		if (localStorage.todolist) {
+			todo.innerHTML = localStorage.todolist;
+			todo2.innerHTML = localStorage.todolist2;
+			todo3.innerHTML = localStorage.todolist3;
+			}
+	}
+	;
+
+})();
+
