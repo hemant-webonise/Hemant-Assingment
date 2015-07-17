28c17b516d3cb0c078ca8d93b51fa7487dcbee82
hemant.bonde <hemant.bonde@weboniselab.com>
2015-07-17 17:43:03 +0530

    BDD Assingment Submission

---
+++ BddAssignment/Jasmine/Funstions.js
@@ -0,0 +1,12 @@
+
+
+
+function AddTwoNumbers(a,b) {
+  return a + b; //Function to return addition of two numbers a and b.
+}
+ 
+function MultiplyTwoNumbers(a,b) {
+	  return a * b; //Function to return product of two numbers a and b.
+}
+
+
